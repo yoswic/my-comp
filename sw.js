fdc9b0089a3d2fda1efde159f7846a4e8f6e823d
yoswic <80913704+yoswic@users.noreply.github.com>
2026-03-06 19:39:14 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'thenotes-v1';
+const ASSETS = [
+  '/my-comp/',
+  '/my-comp/index.html',
+  '/my-comp/manifest.json',
+  '/my-comp/icon-192.png',
+  '/my-comp/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/my-comp/')))
+  );
+});
