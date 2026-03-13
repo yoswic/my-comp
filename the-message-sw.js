0b39db906937c42c64764458406ce84d5285696c
yoswic <80913704+yoswic@users.noreply.github.com>
2026-03-14 00:25:41 +0700

    Add files via upload

---
+++ the-message-sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'the-message-v1';
+const ASSETS = [
+  './the-message.html',
+  './the-message-manifest.json',
+  './message-icon-192.png',
+  './message-icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./the-message.html');
+        }
+      });
+    })
+  );
+});
